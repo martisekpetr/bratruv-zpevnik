21934da3f94fc63bec34cda50e49653e361b6a1d
Petr Martišek <martisekpetr@gmail.com>
2018-10-30 21:56:35 +0100

    Initial commit

---
+++ old_releases/download.php
@@ -0,0 +1,94 @@
+<?php
+$nazev = "download";
+@include("spojeni.inc");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1250">
+<title>KŠMF - Ke stažení</title>
+<link rel="stylesheet" href="styl.css">
+<script language="javascript" src="skripty.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div class="nadpis"></div>
+<div class="telo">
+<div class="left">
+	<div class="menu_nad">Menu</div>
+<?php
+$stranka = "download";
+if(!@include("menu.inc")) echo("<span style=\"font-weight: normal; font-size: 11px;\">Bohužel se někde stala chyba... :(</span>");
+?>
+</div>
+
+<div class="stred">
+<h1>Ke stažení</h1>
+<div class="odkazy">
+    <h3>KŠMF 8</h3>
+	<a href="http://youtu.be/QOrUy3ObrEA" target="_blank">Souborné dílo Williama Shakespeara ve třech minutách</a> &ndash; YouTube<br />
+	<a href="http://youtu.be/JCjwwpKlkhE" target="_blank">Film o filmu: Šejk Spyr</a> &ndash; YouTube<br />
+	<a href="http://youtu.be/jnpAhQT5fQw" target="_blank">Romeo a tma</a> &ndash; YouTube<br />
+	<a href="http://youtu.be/G0JBPVx6XPo" target="_blank">Houmlet, Rómeo a Džúlie</a> &ndash; YouTube<br />
+	<a href="http://youtu.be/9iL1SwjTXQA" target="_blank">Henrieta</a> &ndash; YouTube<br />
+	<a href="http://youtu.be/WMUcMzN46YA" target="_blank">Romeo like a Julie like</a> &ndash; YouTube<br />
+<br /><br />
+    <h3>KŠMF 7</h3>
+	<a href="download/limeriky.pdf">Limeriky</a> (pdf)<br />
+	<a href="download/grafohra-manual.pdf">Grafohra &ndash; manuál</a> (pdf)<br />
+	<a href="download/grafohra-mapa.png">Grafohra &ndash; mapa</a><br />
+	<a href="download/grafohra-logo.jpg">Grafohra &ndash; logo</a>
+<br /><br />
+
+    <h3>KŠMF 2010</h3>
+	<a href="http://ksmf.rajce.idnes.cz" target="_blank">Fotogalerie KŠMF!</a> &ndash; fotky z minulých akcí
+    <br /><br />
+    <a href="http://artax.karlin.mff.cuni.cz/~peltanl/rajce/ksmf2010/">Liborovy fotky</a> <br />
+    <a href="http://www.youtube.com/watch?v=wpVgW4Zq1cQ" target="_blank">"Improvizovaný" tanec na KŠMF 2010</a> &ndash; YouTube
+    <br />
+    <a href="http://www.youtube.com/watch?v=JdRvYBqC6pM" target="_blank">Trailer KŠMF 2010</a> &ndash; YouTube
+    <br /><br />
+    
+    <h3>starší akce</h3>
+    <a href="http://ewi4.rajce.idnes.cz/KSMF_09_25.-28.9.2009/">Eviny fotky z KŠMF 2009 na Rajce.net</a><br>
+    <a href="ke-stazeni/ksmf2008.zip">fotky z KŠMF 2008 ke stažení:</a> - zip, 31 MB<br>
+    <span style="font-size: 90%; line-height: 120%;">Soubor obsahuje fotky od Evy, Jolinar, Lály, Lukáše, Trubuse a Zlosyna. Fotky jsou zmenšené a protříděné (kdybyste cítili, že tam nějaká fotka fakt hodně chybí, napište). Videa tam z kapacitních důvodů nejsou.</span>
+    <br /><br />
+    <a href="http://www.youtube.com/watch?v=zytFs3VaHSc">Scénka <b>Pat a Mat</b> &ndash; KŠMF 2006</a> (YouTube)
+    <br /><br />
+    <hr />
+    <h3 id="zpevnik">Bratrův zpěvník</h3>
+    <h4>Aktuální verze:</h4>
+    <ul> 
+      <li><a href="download/BratruvZpevnik_v4-0.pdf">Bratrův zpěvník v4.0</a><br /></li>
+      <li><a href="download/BratruvZpevnik_v4-0_tisk.pdf">Bratrův zpěvník v4.0</a> - verze pro tisk (A4, 2 strany na list)<br /></li>
+      <li><a href="download/BratruvZpevnik_v4-0_NOVE_tisk.pdf">Bratrův zpěvník v4.0 - nové písně</a> - (verze pro tisk) pouze písničky, které přibyly oproti verzi 3.2 + aktualizovaný obsah</li>
+      <li><a href="download/BratruvZpevnik_v4-0_UPRAVENE_tisk.pdf">Bratrův zpěvník v4.0 - upravené písně</a> - (verze pro tisk) písně, ve kterých došlo k nějakým drobným úpravám</li>
+    </ul>
+    <h4>Starší verze:</h4>
+    <ul>
+      <li><a href="download/BratruvZpevnik_v3-2.pdf">Bratrův zpěvník v3.2</a><br /></li>
+      <li><a href="download/BratruvZpevnik_v3-2_tisk.pdf">Bratrův zpěvník v3.2</a> - verze pro tisk (A4, 2 strany na list)<br /></li>
+      <li><a href="download/BratruvZpevnik_v3-2_UPDATE_tisk.pdf">Bratrův zpěvník v3.2 - update</a> - (verze pro tisk) pouze písničky, které přibyly oproti verzi 3.1 + aktualizovaný obsah + několik změn akordů a dalších drobností</li>
+      <li><a href="download/BratruvZpevnik_v3-1.pdf">Bratrův zpěvník v3.1</a><br /></li>
+      <li><a href="download/BratruvZpevnik_v3-1_tisk.pdf">Bratrův zpěvník v3.1</a> - verze pro tisk (A4, 2 strany na list)<br /></li>
+      <li><a href="download/BratruvZpevnik_v3-1_UPDATE_tisk.pdf">Bratrův zpěvník v3.1 - update</a> - (verze pro tisk) pouze písničky, které přibyly oproti verzi 3.0 + aktualizovaný obsah</li>
+      <li><a href="download/BratruvZpevnik_v3-0.pdf">Bratrův zpěvník v3.0</a><br /></li>
+      <li><a href="download/BratruvZpevnik_v3-0_tisk.pdf">Bratrův zpěvník v3.0</a> - verze pro tisk (A4, 2 strany na list)<br /></li>
+      <li><a href="download/BratruvZpevnik_v3-0_UPDATE_tisk.pdf">Bratrův zpěvník v3.0 - update</a> - (verze pro tisk) pouze písničky, které přibyly oproti verzi 2.0 + aktualizovaný obsah</li>
+      <li><a href="download/BratruvZpevnik_v2-0.pdf">Bratrův zpěvník v2.0</a><br /></li>
+      <li><a href="download/BratruvZpevnik_v2-0_UPDATE.pdf">Bratrův zpěvník v2.0 - update</a> - pouze písničky, které přibyly oproti verzi 1.1 + aktualizovaný obsah</li>
+      <li><a href="download/BratruvZpevnik_v1-1.pdf">Bratrův zpěvník v1.1</a><br /></li>
+      <li><a href="download/BratruvZpevnik_v1-0.pdf">Bratrův zpěvník v1.0</a><br /></li>
+    </ul>
+        <br />
+    <hr />
+    <h3 id="thymesup">Thyme's Up</h3>
+    <a href="download/ThymesUp.apk">Thyme's Up 1.1</a>
+
+</div>
+</div>
+
+</div>
+</body>
+</html>
