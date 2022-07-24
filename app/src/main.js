85a3177a62d270edb0ec05ad782cd8039b2c37fe
Petr Martišek <martisekpetr@gmail.com>
2022-07-24 11:10:31 +0200

    feat: add PWA capabilities

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,7 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
