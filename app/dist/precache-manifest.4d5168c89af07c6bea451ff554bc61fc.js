5141a5a403067402d29da344213afa11a8437b17
Petr Martišek <martisekpetr@gmail.com>
2022-07-28 23:58:59 +0200

    chore: add dist to git

---
+++ app/dist/precache-manifest.4d5168c89af07c6bea451ff554bc61fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5feee1b272da005ed82f",
+    "url": "/css/app.111134d9.css"
+  },
+  {
+    "revision": "b19ca3595c2c026e7981",
+    "url": "/css/chunk-vendors.03fb03c0.css"
+  },
+  {
+    "revision": "db0212fbf7207a0c323304fc81244618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5feee1b272da005ed82f",
+    "url": "/js/app.c169b596.js"
+  },
+  {
+    "revision": "b19ca3595c2c026e7981",
+    "url": "/js/chunk-vendors.2823238e.js"
+  },
+  {
+    "revision": "9f071f4fd76f8bf31602e591da0c25ba",
+    "url": "/static/browserconfig.xml"
+  },
+  {
+    "revision": "f6f6de06a1543038ab72d18de88c6dbc",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/static/robots.txt"
+  }
+]);
